95c16cddcfd563ed2ebdaf3f3b0b8a9bddc8a5df
Alvaro-GT <alvaro.galeote@nitbit.mx>
2024-12-10 14:49:20 -0600

    Add filtering for alive characters using filter

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 async function fetchRickAndMortyData() {
     try {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-        
-        if (!response.ok) {
-            throw new Error(`Error al consumir la API: ${response.status}`);
-        }
-    
-        const data = await response.json();
-        console.log("Recibiendo datos:", data.results);
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+  
+      if (!response.ok) {
+        throw new Error(`Error al consumir la API: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      //console.log("Recibiendo datos:", data.results);
 
+      const aliveCharacters = data.results.filter(character => character.status === "Alive");
+      
+      console.log("Personajes con Alive:", aliveCharacters);
     } catch (error) {
       console.error("Error:", error.message);
     }
